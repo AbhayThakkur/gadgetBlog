3a1e01b489ebfc0be2c614d8a03c1de548d1e639
AbhayThakkur <abhaythakurpy@gmail.com>
2024-12-19 17:18:55 +0530

    fix static files and media files for production

--- gadgetBlog/settings.py
+++ gadgetBlog/settings.py
@@ -123,18 +123,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static/home",
-    BASE_DIR / "static/blog",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+# Media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Static files configuration
+STATIC_URL = '/static/'
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static/home'),
+                os.path.join(BASE_DIR, 'static/blog')
+            ]
+
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
