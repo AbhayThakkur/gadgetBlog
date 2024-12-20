8b962e1acc7caf8ab21a80c018413aeff172ad56
AbhayThakkur <abhaythakurpy@gmail.com>
2024-12-20 19:37:17 +0530

    debug is true and add first content

--- gadgetBlog/settings.py
+++ gadgetBlog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n9ekxx7x+kth6ljn7fdk2wyn8zyj*n1x+)0zj69fxpd!2c9^7@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['gadgetblog.onrender.com', 'https://gadgetblog.onrender.com/','*']
 
